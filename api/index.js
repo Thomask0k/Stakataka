c23d5971243feaa553ad5d58803fea7ea953388f
Thomas Kok <salt-dev@salt-devs-MacBook-Air-2.local>
2022-11-09 21:55:05 +0100

    added api with babel

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
-console.log('express app');
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({
+    version: "0.0.1",
+    name: "stakataka",
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Starting server on localhost:3000`);
+});
